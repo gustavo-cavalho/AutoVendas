ba78a96429e52a3acc2cc00b4a54175dc154c412
Gustavo Oliveira <gustavoooliveiradecarvalho@gmail.com>
2025-01-26 22:15:00 -0400

    refac: write a redable code

--- src/Traits/JsonResponseUtil.php
+++ src/Traits/JsonResponseUtil.php
@@ -2,64 +2,68 @@
 
 namespace App\Traits;
 
-use App\Interfaces\MultipleErrosInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Serializer\SerializerInterface;
 
 trait JsonResponseUtil
 {
-    private SerializerInterface $serializer;
-
-    private function successResponse(string $message, ?object $object = null, int $status = Response::HTTP_OK)
+    private function successResponse(string $message, $data = [], int $status = Response::HTTP_OK): JsonResponse
     {
-        $json = [
+        $jsonResponse = [
             'message' => $message,
         ];
-        $object ? $json['data'] = $object : null;
 
-        return $this->json($json, $status);
-    }
-
-    private function failureResponse(string $message, ?array $err = null, int $status = Response::HTTP_BAD_REQUEST)
-    {
-        $json = ['message' => $message];
-
-        if ($err) {
-            $json['errors'] = $err;
+        if (!empty($data)) {
+            $jsonResponse['data'] = $data;
         }
 
-        return $this->json($json, $status);
+        return $this->json($jsonResponse, $status);
     }
 
-    ///// Success returns /////
-    public function statusOk(string $message, ?object $object = null)
+    private function failureResponse(string $message, array $errors = [], int $status = Response::HTTP_BAD_REQUEST): JsonResponse
     {
-        return $this->successResponse($message, $object);
+        $jsonResponse = [
+            'message' => $message,
+        ];
+
+        if (!empty($errors)) {
+            $jsonResponse['errors'] = $errors;
+        }
+
+        return $this->json($jsonResponse, $status);
     }
 
-    public function statusCreated(string $message, ?object $object = null)
+    // /// Success Response /////
+
+    public function statusOk(string $message, $data = []): JsonResponse
     {
-        return $this->successResponse($message, $object, Response::HTTP_CREATED);
+        return $this->successResponse($message, $data, Response::HTTP_OK);
     }
 
-    public function statusNoContent(string $message)
+    public function statusCreated(string $message, $data = []): JsonResponse
     {
-        return $this->successResponse($message, null, Response::HTTP_NO_CONTENT);
+        return $this->successResponse($message, $data, Response::HTTP_CREATED);
     }
 
-    ///// Errors returns /////
-    public function errBadRequest(string $message, ?array $err = null)
+    public function statusNoContent(string $message): JsonResponse
     {
-        return $this->failureResponse($message, $err, Response::HTTP_BAD_REQUEST);
+        return $this->successResponse($message, [], Response::HTTP_NO_CONTENT);
     }
 
-    public function errNotFound(string $message)
+    // /// Errors Response /////
+
+    public function errBadRequest(string $message, array $errors = []): JsonResponse
     {
-        return $this->failureResponse($message, null, Response::HTTP_NOT_FOUND);
+        return $this->failureResponse($message, $errors, Response::HTTP_BAD_REQUEST);
     }
 
-    public function errUnauthorized(string $message)
+    public function errNotFound(string $message): JsonResponse
     {
-        return $this->failureResponse($message, null, Response::HTTP_UNAUTHORIZED);
+        return $this->failureResponse($message, [], Response::HTTP_NOT_FOUND);
+    }
+
+    public function errUnauthorized(string $message): JsonResponse
+    {
+        return $this->failureResponse($message, [], Response::HTTP_UNAUTHORIZED);
     }
 }
