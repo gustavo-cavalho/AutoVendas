b63c362461929b0062ff69e94133dd8cbcf3f9f4
Gustavo Carvalho <gustavo.carvalho@pigz.com.br>
2025-01-27 13:05:07 -0300

    faet: add Vehicles entity
    - must set the relations between entities
    - must add more vehicle infos

---
+++ src/Entity/Vehicle.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VehicleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VehicleRepository::class)
+ */
+class Vehicle
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $brand;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $yearOfProduction;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $licensePlate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getYearOfProduction(): ?string
+    {
+        return $this->yearOfProduction;
+    }
+
+    public function setYearOfProduction(string $yearOfProduction): self
+    {
+        $this->yearOfProduction = $yearOfProduction;
+
+        return $this;
+    }
+
+    public function getLicensePlate(): ?string
+    {
+        return $this->licensePlate;
+    }
+
+    public function setLicensePlate(string $licensePlate): self
+    {
+        $this->licensePlate = $licensePlate;
+
+        return $this;
+    }
+}
