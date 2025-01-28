54217a6dffe1ba446ac160c23fe7738a56ef06ce
Gustavo Carvalho <gustavo.carvalho@pigz.com.br>
2025-01-28 10:54:25 -0300

    fix: directoris names

---
+++ src/Interfaces/Auth/RegistrationsServiceInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Interfaces\Auth;
+
+use App\DTO\UserDTO;
+use App\Entity\User;
+use App\Exceptions\IdentityAlreadyExistsException;
+
+/**
+ * Contains methods to register a new user.
+ */
+interface RegistrationsServiceInterface
+{
+    /**
+     * Register a new user and hashes the password.
+     *
+     * @param UserDTO $userDTO the user to be registered
+     *
+     * @return User the registered user Entity
+     *
+     * @throws IdentityAlreadyExistsException if the user already exists
+     */
+    public function register(UserDTO $userDTO): User;
+}
