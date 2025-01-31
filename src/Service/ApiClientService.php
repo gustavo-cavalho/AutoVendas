5becddd46c50d86ffbb0a162b03bd592d25420cc
Gustavo Carvalho <gustavo.carvalho@pigz.com.br>
2025-01-31 15:24:05 -0300

    refac: change files and classes names

--- src/Service/ApiClientService.php
+++ src/Service/ApiClientService.php
@@ -4,7 +4,7 @@ namespace App\Service;
 
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-class APIClientService
+class ApiClientService
 {
     private HttpClientInterface $httpClient;
     private string $baseUrl;
